89b572fab9565b15210c1e16a14ddc8024b8aafc
GARAYYY <jongaray7@gmail.com>
2024-09-18 19:15:46 +0200

    Pagina web subida. V2.0

---
+++ script.js
@@ -0,0 +1,26 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100; // Calcular el desplazamiento del slider
+    document.querySelector('.slider').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+// Mostrar la primera imagen al cargar la página
+showSlide(currentIndex);
