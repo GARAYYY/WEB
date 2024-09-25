beb40eda2406619fde3d6987a5059f76b6d5364b
GARAYYY <jongaray7@gmail.com>
2024-09-25 15:34:06 +0200

    Adaptacion de scripts en varios

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-let currentIndex = 0;
-
-function showSlide(index) {
-  const slides = document.querySelectorAll(".slide");
-  const totalSlides = slides.length;
-  if (index >= totalSlides) {
-    currentIndex = 0;
-  } else if (index < 0) {
-    currentIndex = totalSlides - 1;
-  } else {
-    currentIndex = index;
-  }
-  const offset = -currentIndex * 100;
-  document.querySelector(".slider").style.transform = `translateX(${offset}%)`;
-}
-
-function nextSlide() {
-  showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-  showSlide(currentIndex - 1);
-}
-
-showSlide(currentIndex);
-
 document.addEventListener("DOMContentLoaded", function () {
   const menuHamburguesa = document.querySelector(".menu-hamburguesa");
   const navMenu = document.querySelector("nav ul");
